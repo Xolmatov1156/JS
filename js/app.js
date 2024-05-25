298ee99431436db80a9ae5d148ff9eecd8b7ddcd
Xolmatov1156 <xolmatov1156@gmail.com>
2024-05-25 21:37:46 +0500

    js

---
+++ js/app.js
@@ -0,0 +1,46 @@
+var task1 = prompt("Audio va video elementlarini ovozsiz qilish imkoniyatini qaysi atribut qo'shib beradi?")
+var task2 = prompt("Link, meta, style va hokazo kabi bosh teglar qaysi teg ichida joylashishi kerak?")
+var task3 = prompt("Qaysi teg yordamida tartibli ro'yhat yaratishimiz mumkin?")
+var task4 = prompt("HTMLda ishlatiladigan belgilovchi setni tasdiqlash uchun, qaysi kod ishlatiladi?")
+var task5 = prompt("HTMLda matn va paragraflar qaysi teg bilan beriladi?")
+var task5 = prompt("Inputga malumot kiritib bo'lmaydigan qilish uchun qaysi atributdan foydalanamiz?")
+var task6 = prompt("Select ichiga malumotni guruhlashda qaysi tegdan foydalanamiz?")
+var task7 = prompt("Labelni va inputga qanday bog'laymiz?")
+var task8 = prompt("Padding: 25px 75px 100px; Tepa tarafdan qancha padding mavjud.")
+var task9 = prompt("Jadvalni toq qatorlarini stilini berishimiz uchun nima qilishimiz kerak?")
+var task10 = prompt("Qaysi css xossasi yordamida matnni qalin yoki ingichka qilishimiz mumkin?")
+var task11 = prompt("Uzuk chiziqli border qanday yoziladi?")
+var task12 = prompt("Qaysi css xossasi yordamida elementga balandlik berishimiz mumkin?")
+var task13 = prompt("Qaysi css xossasi yordamida matnga soya berishimiz mumkin?")
+var task14 = prompt("RGB ranglarini qaysi sonlar orqali berishimiz mumkin?")
+var task15 = prompt("CSS uslublar sahifasi faylini nima nomlashim kerak?")
+var task16 = prompt("Veb-sahifadagi fon tasvirini qanday qilib uzaytirasiz?")
+var task17 = prompt("CSS tanlovchilari uchun vergul nima?")
+var task18 = prompt("ID identifikatorini qachon o'zgartirishi kerak?")
+var task19 = prompt("CSS-ni qo'shish uchun @ import va link o'rtasidagi farq nima?")
+var task20 = prompt("Standart ulanish ranglarini qanday bekor qilasiz?")
+
+var result = {
+    task1:task1, 
+    task2:task2, 
+    task3:task3, 
+    task4:task4, 
+    task5:task5, 
+    task6:task6, 
+    task7:task7, 
+    task8:task8, 
+    task9:task9, 
+    task10:task10, 
+    task11:task11, 
+    task12:task12, 
+    task13:task13, 
+    task14:task14, 
+    task15:task15,
+    task15:task16,
+    task15:task17,
+    task15:task18,
+    task15:task19,
+    task15:task20,
+}
+
+console.log(result);
